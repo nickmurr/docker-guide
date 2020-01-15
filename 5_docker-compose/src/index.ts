46fe3cb6f5167a00f9645178f4537b02eeb7362f
nickmurr <32221055+NickMurr@users.noreply.github.com>
2020-01-15 13:52:24 +0200

    cra

--- 5_docker-compose/src/index.ts
+++ 5_docker-compose/src/index.ts
@@ -1,24 +1,24 @@
-import express from 'express';
-import { createClient } from 'redis';
-import process from 'process';
+import express from "express";
+import { createClient } from "redis";
+// import process from "process";
 
 const app = express();
 
 const client = createClient({
-  host: 'redis-server',
+  host: "redis-server",
   port: 6379
 });
 
-client.set('visits', '0');
+client.set("visits", "1");
 
-app.get('/', (req, res) => {
-  client.get('visits', (err, visits: string) => {
+app.get("/", (req, res) => {
+  client.get("visits", (err, visits: string) => {
     res.send(`Number of visits: ${visits}`);
     const v = parseInt(visits) + 1;
-    client.set('visits', `${v}`);
+    client.set("visits", `${v}`);
   });
 });
 
 app.listen(8081, () => {
-  console.log('Listening on port 8081');
+  console.log("Listening on port 8081");
 });
