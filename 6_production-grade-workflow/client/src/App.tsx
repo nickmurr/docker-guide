04e2aace793920efb6b251ba0f9a437268f6bd7d
nickmurr <nickmur72@gmail.com>
2020-01-15 16:10:34 +0200

    implementing multi step build

--- 6_production-grade-workflow/client/src/App.tsx
+++ 6_production-grade-workflow/client/src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 const App: React.FC = () => {
   return (
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi there
+          Learn react
         </a>
       </header>
     </div>
